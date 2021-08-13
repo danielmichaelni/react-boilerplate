2111f2b8d3636de1fc1d624ceb14348def0c2c39
danielmichaelni <danielmichaelni@gmail.com>
2021-08-13 00:08:55 -0400

    rm antd styled-components stylelint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from 'next/app';
 import { ReactElement } from 'react';
 
-import 'antd/dist/antd.dark.css';
 import 'styles/index.css';
 
 export const App = ({ Component, pageProps }: AppProps): ReactElement => (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,5 @@
-import { Layout as BaseLayout, Space } from 'antd';
 import Head from 'next/head';
 import { ReactElement } from 'react';
-import styled from 'styled-components';
-
-const Layout = styled(BaseLayout).attrs({ className: 'min-h-screen' })``;
-
-const Header = styled(BaseLayout.Header).attrs({
-  className: 'border-b border-gray-800 flex items-center',
-})`
-  background: unset;
-`;
-
-const Content = styled(BaseLayout.Content).attrs({
-  className: 'm-auto max-w-6xl py-12',
-})``;
-
-const Footer = styled(BaseLayout.Footer).attrs({
-  className: 'border-t border-gray-800',
-})``;
-
-const Logo = styled.div.attrs({
-  className: 'font-semibold text-lg text-white',
-})``;
 
 const IndexPage = (): ReactElement => (
   <>
@@ -29,41 +7,36 @@ const IndexPage = (): ReactElement => (
       <title>Index</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Layout>
-      <Header>
-        <Logo>React Boilerplate</Logo>
-      </Header>
-      <Content>
-        <Space direction="vertical">
-          <div>
-            I created this boilerplate because I found myself doing the same
-            things over and over again every time I started a new React project.
-          </div>
-          <div>
-            The boilerplate is built on top of Next.js and uses TypeScript.
-          </div>
-          <div>
-            I added a few libraries I like to use in almost every project:
-          </div>
-          <ul>
-            <li>Ant Design</li>
-            <li>Styled Components</li>
-            <li>Tailwind CSS</li>
-          </ul>
-          <div>I added some tooling to improve developer efficiency:</div>
-          <ul>
-            <li>ESLint</li>
-            <li>Prettier</li>
-            <li>Stylelint</li>
-          </ul>
-        </Space>
-      </Content>
-      <Footer>
-        <div>
-          Made by <a href="https://github.com/danielmichaelni">Daniel Ni</a>
+    <div className="flex flex-col min-h-screen">
+      <div className="border-b border-gray-200 flex items-center p-6">
+        <div className="font-medium text-xl">React Boilerplate</div>
+      </div>
+      <div className="flex-grow m-auto max-w-6xl py-12">
+        <div className="mb-2">
+          I created this boilerplate because I found myself doing the same
+          things over and over again every time I started a new React project.
         </div>
-      </Footer>
-    </Layout>
+        <div className="mb-2">
+          The boilerplate is built on top of Next.js and uses TypeScript.
+        </div>
+        <div className="mb-2">
+          I added a few libraries I like to use in almost every project:
+        </div>
+        <ul className="mb-2">
+          <li>- Tailwind CSS</li>
+        </ul>
+        <div className="mb-2">
+          I added some tooling to improve developer efficiency:
+        </div>
+        <ul className="mb-2">
+          <li>- ESLint</li>
+          <li>- Prettier</li>
+        </ul>
+      </div>
+      <div className="border-t border-gray-200 p-6">
+        Made by <a href="https://github.com/danielmichaelni">Daniel Ni</a>
+      </div>
+    </div>
   </>
 );
 
